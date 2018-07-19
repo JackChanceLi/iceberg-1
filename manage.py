ab62ad8a43b004fe6c2a0ced488c4fe23a43e0ab
CWT <cwt9708@163.com>
2018-07-20 00:37:39 +0800

    minor fix

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-manager.add_command('runserver', Server(host='127.0.0.1', port=8070))
+manager.add_command('runserver', Server(host='0.0.0.0', port=8070))
 
 if __name__ == '__main__':
     manager.run()
